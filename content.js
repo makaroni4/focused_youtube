6a8e50de3aa1e2c8752e0b042d438759d053ed67
Anatoli Makarevich <makaroni4@gmail.com>
2018-06-25 23:31:18 +0200

    Add simple Vue app

--- content.js
+++ content.js
@@ -1,5 +1,41 @@
-$("#contents").remove();
-$(".style-scope.ytd-browse").remove();
-$("#primary").remove();
+(function() {
+  if(window.location.pathname !== "/") {
+    return;
+  }
 
-$("body").html("<div class='focused-youtube'>Welcome youtube</div>")
+  const body = document.querySelector("body");
+  const anchor = document.createElement("div");
+  anchor.id = "mega-app"
+
+  body.innerHTML = "";
+  document.body.appendChild(anchor);
+
+  var app = new Vue({
+    el: "#mega-app",
+    data: {
+      header: "Search Youtube",
+      searchQuery: ""
+    },
+    methods: {
+      search: function (event) {
+        event.preventDefault();
+
+        window.location.href = "https://www.youtube.com/results?search_query=" + encodeURI(this.searchQuery);
+      }
+    },
+    template: `
+      <div class="focused-youtube">
+        <div class="focused-youtube__body">
+          <div class="focused-youtube__header">
+            {{ header }}
+          </div>
+
+          <form class="focused-youtube__form" action="#" v-on:submit="search">
+            <input type="text" v-model="searchQuery" />
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+      </div>
+    `
+  });
+})();

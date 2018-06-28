bcf8cefe94a9e7b36df259bdbd5f0adc7066fb97
Anatoli Makarevich <makaroni4@gmail.com>
2018-06-28 23:36:09 +0200

    Extract JS to folder

--- js/app.js
+++ js/app.js
@@ -69,19 +69,23 @@
     });
   }
 
-  var observeDOM = (function(){
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-        eventListenerSupported = window.addEventListener;
+  var observeDOM = (function() {
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    var eventListenerSupported = window.addEventListener;
 
-    return function(obj, callback){
-      if(MutationObserver){
-        var obs = new MutationObserver(function(mutations, observer){
-            if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
-                callback();
+    return function(obj, callback) {
+      if(MutationObserver) {
+        var obs = new MutationObserver(function(mutations, observer) {
+          if(mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
+            callback();
+          }
         });
 
-        obs.observe( obj, { childList:true, subtree:true });
-      } else if(eventListenerSupported){
+        obs.observe(obj, {
+          childList:true,
+          subtree:true
+        });
+      } else if(eventListenerSupported) {
         obj.addEventListener("DOMNodeInserted", callback, false);
         obj.addEventListener("DOMNodeRemoved", callback, false);
       }

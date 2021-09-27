2505aeb7dbc1cff280c1b2efec0ae395ba27f86c
Anatoli Makarevich <makaroni4@gmail.com>
2021-09-27 19:45:36 +0200

    ES6

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 (function() {
   document.body.style.display = "block";
 
-  var currentUrl = window.location.href;
+  let currentUrl = window.location.href;
 
-  var cleanUpFYClasses = function() {
+  let cleanUpFYClasses = () => {
     document.body.classList.forEach(className => {
       if(className.startsWith("fy-")) {
         document.body.classList.remove(className);
@@ -11,7 +11,7 @@
     });
   }
 
-  var initFY = function() {
+  const initFY = () => {
     cleanUpFYClasses();
 
     if(window.location.pathname === "/") {
@@ -23,21 +23,22 @@
     }
   }
 
-  var initWatchPage = function() {
+  const initWatchPage = () => {
     document.body.classList.add("fy-watch-page");
   }
 
-  var initResultsPage = function() {
+  const initResultsPage = () => {
     document.body.classList.add("fy-results-page");
   }
 
-  var initHomePage = function() {
-	function search(event) {
-	  event.preventDefault();
+  const initHomePage = () => {
+    const search = (event) => {
+      event.preventDefault();
 
-	  window.location.href = "https://www.youtube.com/results?search_query=" + encodeURIComponent(anchor.querySelector('.search-form__text-input').value);
-	}
-	  
+      const query = anchor.querySelector(".search-form__text-input").value;
+      window.location.href = "https://www.youtube.com/results?search_query=" + encodeURIComponent(query);
+    }
+
     document.body.classList.add("fy-home-page");
 
     const body = document.querySelector("body");
@@ -46,37 +47,39 @@
 
     body.innerHTML = "";
     document.body.appendChild(anchor);
-	anchor.innerHTML = `
-        <div class="focused-youtube">
-          <div class="focused-youtube__logo">
-          </div>
 
-          <div class="focused-youtube__body">
-            <form class="focused-youtube__form search-form" action="#">
-              <input class="search-form__text-input" type="text" placeholder="Search" />
-              <button class="search-form__submit"></button>
-            </form>
-          </div>
+    anchor.innerHTML = `
+      <div class="focused-youtube">
+        <div class="focused-youtube__logo">
         </div>
-      `;
-	  anchor.querySelector('.search-form').onsubmit = search;
+
+        <div class="focused-youtube__body">
+          <form class="focused-youtube__form search-form" action="#">
+            <input class="search-form__text-input" type="text" placeholder="Search" />
+            <button class="search-form__submit"></button>
+          </form>
+        </div>
+      </div>
+    `;
+
+    anchor.querySelector(".search-form").onsubmit = search;
   }
 
-  var observeDOM = (function() {
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-    var eventListenerSupported = window.addEventListener;
+  const observeDOM = (function() {
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    const eventListenerSupported = window.addEventListener;
 
     return function(obj, callback) {
       if(MutationObserver) {
-        var obs = new MutationObserver(function(mutations, observer) {
+        let obs = new MutationObserver(function(mutations, observer) {
           if(mutations[0].addedNodes.length || mutations[0].removedNodes.length) {
             callback();
           }
         });
 
         obs.observe(obj, {
-          childList:true,
-          subtree:true
+          childList: true,
+          subtree: true
         });
       } else if(eventListenerSupported) {
         obj.addEventListener("DOMNodeInserted", callback, false);
@@ -90,6 +93,7 @@
   observeDOM(document.body, function(){
     if(currentUrl !== window.location.href) {
       currentUrl = window.location.href;
+
       initFY();
     }
   });

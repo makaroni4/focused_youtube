5ba7292a768531dc8d3216b9e5e32a1fdad4484d
Anatoli Makarevich <makaroni4@gmail.com>
2023-01-29 20:58:27 +0100

    Show toggle switch only on pages where we run FY

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@
 
   const initFY = () => {
     cleanUpFYClasses();
-    initToggleSwitch();
 
     if (window.location.pathname === "/") {
       initHomePage();
@@ -21,15 +20,69 @@
       initResultsPage();
     } else if (window.location.pathname === "/watch") {
       initWatchPage();
+    } else {
+      initDefaultPage();
     }
+  }
+
+  // Pages where we don't hide comments or recommendations, like stories or profile pages.
+  const initDefaultPage = () => {
+    const toggleSwitch = document.querySelector(".fy-switch");
+
+    if(toggleSwitch) {
+      toggleSwitch.remove();
+    }
+  }
+
+  const initToggleSwitch = () => {
+    const container = document.querySelector("#start.ytd-masthead");
+
+    if (!container) {
+      return;
+    }
+
+    const toggleSwitch = document.querySelector(".fy-switch");
+
+    if (toggleSwitch) {
+      // This is desired UX: turn switch ON on every page load.
+      const input = toggleSwitch.querySelector("input");
+      input.checked = true;
+
+      return;
+    }
+
+    const label = document.createElement("label");
+    label.classList.add("fy-switch");
+    container.appendChild(label);
+
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.checked = true;
+    label.appendChild(input);
+
+    const span = document.createElement("span");
+    span.classList.add("fy-switch__slider");
+    label.appendChild(span);
+
+    input.addEventListener("change", function () {
+      if (this.checked) {
+        initFY();
+      } else {
+        cleanUpFYClasses();
+      }
+    });
   }
 
   const initWatchPage = () => {
     document.body.classList.add("fy-watch-page");
+
+    initToggleSwitch();
   }
 
   const initResultsPage = () => {
     document.body.classList.add("fy-results-page");
+
+    initToggleSwitch();
   }
 
   const initHomePage = () => {
@@ -88,45 +141,6 @@
       }
     };
   })();
-
-  const initToggleSwitch = () => {
-    const container = document.querySelector("#start.ytd-masthead");
-
-    if (!container) {
-      return;
-    }
-
-    const toggleSwitch = document.querySelector(".fy-switch");
-
-    if (toggleSwitch) {
-      // This is desired UX: turn switch ON on every page load.
-      const input = toggleSwitch.querySelector("input");
-      input.checked = true;
-
-      return;
-    }
-
-    const label = document.createElement("label");
-    label.classList.add("fy-switch");
-    container.appendChild(label);
-
-    const input = document.createElement("input");
-    input.type = "checkbox";
-    input.checked = true;
-    label.appendChild(input);
-
-    const span = document.createElement("span");
-    span.classList.add("fy-switch__slider");
-    label.appendChild(span);
-
-    input.addEventListener("change", function () {
-      if (this.checked) {
-        initFY();
-      } else {
-        cleanUpFYClasses();
-      }
-    });
-  }
 
   initFY();
 

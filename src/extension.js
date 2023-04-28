2f739ab354ddfaaa60c30161d07b831a31408b2e
Anatoli Makarevich <makaroni4@gmail.com>
2023-04-29 00:54:20 +0200

    Listening to key update

--- src/extension.js
+++ src/extension.js
@@ -153,4 +153,13 @@ import './style-overrides.css'
       initFY();
     }
   });
+
+  chrome.storage.onChanged.addListener((changes, namespace) => {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+      console.log(
+        `Storage key "${key}" in namespace "${namespace}" changed.`,
+        `Old value was "${oldValue}", new value is "${newValue}".`
+      );
+    }
+  });
 })();

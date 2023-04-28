9c097ad886c79e4777137850dfe5117c3f3adeb6
Anatoli Makarevich <makaroni4@gmail.com>
2023-04-28 23:19:31 +0200

    Set up Vue app

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: "src/popup.js",
+        extension: "src/extension.js"
+      },
+      output: {
+        entryFileNames: "[name].js"
+      }
+    }
+  }
+})

f1b2730346c88ad18df154e3ab2b96f1deb0dc05
Anatoli Makarevich <makaroni4@gmail.com>
2023-04-29 22:22:57 +0200

    Added a duplication comment

--- src/js/chrome-storage.js
+++ src/js/chrome-storage.js
@@ -1,3 +1,8 @@
+// IMPORTANT: the SETTINGS_COMMENTS_KEY and readStorageData are
+// duplicated in the extension.js file to avoid using modules. An extension
+// with an import statement fails with It fails with "Can't use import
+// statement outside a module".
+
 export const SETTINGS_COMMENTS_KEY = "settings:comments"
 
 export const writeStorageData = (storageKey, value, callback) => {

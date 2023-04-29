d12e57853c46c86e331901f99d9374fc1a8638bc
Anatoli Makarevich <makaroni4@gmail.com>
2023-04-29 14:06:21 +0200

    Remove legacy switch

--- src/extension.js
+++ src/extension.js
@@ -23,57 +23,7 @@ const initFY = () => {
     initResultsPage();
   } else if (window.location.pathname === "/watch") {
     initWatchPage();
-  } else {
-    initDefaultPage();
   }
-}
-
-// Pages where we don't hide comments or recommendations, like stories or profile pages.
-const initDefaultPage = () => {
-  const toggleSwitch = document.querySelector(".fy-switch");
-
-  if(toggleSwitch) {
-    toggleSwitch.remove();
-  }
-}
-
-const initToggleSwitch = () => {
-  const container = document.querySelector("#start.ytd-masthead");
-
-  if (!container) {
-    return;
-  }
-
-  const toggleSwitch = document.querySelector(".fy-switch");
-
-  if (toggleSwitch) {
-    // This is desired UX: turn switch ON on every page load.
-    const input = toggleSwitch.querySelector("input");
-    input.checked = true;
-
-    return;
-  }
-
-  const label = document.createElement("label");
-  label.classList.add("fy-switch");
-  container.appendChild(label);
-
-  const input = document.createElement("input");
-  input.type = "checkbox";
-  input.checked = true;
-  label.appendChild(input);
-
-  const span = document.createElement("span");
-  span.classList.add("fy-switch__slider");
-  label.appendChild(span);
-
-  input.addEventListener("change", function () {
-    if (this.checked) {
-      initFY();
-    } else {
-      cleanUpFYClasses();
-    }
-  });
 }
 
 const initWatchPage = () => {
@@ -100,14 +50,10 @@ const initWatchPage = () => {
       $body.classList.remove("fy-comments-active");
     }
   });
-
-  initToggleSwitch();
 }
 
 const initResultsPage = () => {
   document.body.classList.add("fy-results-page");
-
-  initToggleSwitch();
 }
 
 const initHomePage = () => {

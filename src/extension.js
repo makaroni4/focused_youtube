183a1b639a528b42d4da86c89376919e574cea3f
Anatoli Makarevich <makaroni4@gmail.com>
2024-09-05 20:03:20 +0200

    Added Playlists menu item

--- src/extension.js
+++ src/extension.js
@@ -90,6 +90,8 @@ const mountLogoMenu = () => {
       <div class="fy-logo-menu__links">
         <a href="/feed/history" class="fy-logo-menu__link">Watch history</a>
 
+        <a href="/feed/playlists" class="fy-logo-menu__link">Playlists</a>
+
         <a href="/playlist?list=WL" class="fy-logo-menu__link">Watch later</a>
 
         <a href="/playlist?list=LL" class="fy-logo-menu__link">Liked videos</a>

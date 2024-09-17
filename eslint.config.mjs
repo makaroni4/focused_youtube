1f22547116cafe2da7e9eb158c0b6203cfbbaa4d
thesolomon-tech <140577015+thesolomon-tech@users.noreply.github.com>
2024-09-17 08:30:57 +0000

    Updated config and packages

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    ...compat.extends("eslint:recommended", "plugin:vue/vue3-recommended", "prettier"),
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                chrome: true,
+                observer: true,
+            },
+        },
+
+        rules: {
+            "vue/multi-word-component-names": "off",
+            quotes: ["error", "double"],
+            semi: ["error", "never"],
+        },
+    },
+];

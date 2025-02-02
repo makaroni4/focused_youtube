7fb811d59335a010f9d578fd2b482d7383c126fc
Anatoli Makarevich <makaroni4@gmail.com>
2025-02-02 19:38:36 +0100

    Added icons for hover menu

--- src/components/logo-menu.js
+++ src/components/logo-menu.js
@@ -21,17 +21,17 @@ export const mountLogoMenu = () => {
   menu.innerHTML = `
     <div class="fy-logo-menu__body">
       <div class="fy-logo-menu__links">
-        <a href="/feed/subscriptions" class="fy-logo-menu__link">Subscriptions</a>
+        <a href="/feed/subscriptions" class="fy-logo-menu__link fy-logo-menu__link--sub">Subscriptions</a>
 
-        <a href="/feed/history" class="fy-logo-menu__link">Watch history</a>
+        <a href="/feed/history" class="fy-logo-menu__link fy-logo-menu__link--history">Watch history</a>
 
-        <a href="/feed/playlists" class="fy-logo-menu__link">Playlists</a>
+        <a href="/feed/playlists" class="fy-logo-menu__link fy-logo-menu__link--playlists">Playlists</a>
 
-        <a href="/playlist?list=WL" class="fy-logo-menu__link">Watch later</a>
+        <a href="/playlist?list=WL" class="fy-logo-menu__link fy-logo-menu__link--watch-later">Watch later</a>
 
-        <a href="/playlist?list=LL" class="fy-logo-menu__link">Liked videos</a>
+        <a href="/playlist?list=LL" class="fy-logo-menu__link fy-logo-menu__link--liked-videos">Liked videos</a>
 
-        <a href="/account" class="fy-logo-menu__link">Account</a>
+        <a href="/account" class="fy-logo-menu__link fy-logo-menu__link--settings">Account</a>
       </div>
     </div>
   `

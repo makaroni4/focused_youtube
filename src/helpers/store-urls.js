8e7dde27dd4d661fcf534d12f1c56eb0ee52e1b4
Anatoli Makarevich <makaroni4@gmail.com>
2025-03-03 07:34:29 +0100

    Added Edge extension URL

--- src/helpers/store-urls.js
+++ src/helpers/store-urls.js
@@ -2,14 +2,14 @@
 
 const storeUrls = {
   chromium: "https://chromewebstore.google.com/detail/focused-youtube/nfghbmabdoakhobmimnjkamfdnpfammn",
-  firefox: "https://addons.mozilla.org/en-US/firefox/addon/focused-youtube/",
-  edge: "https://microsoftedge.microsoft.com/addons/detail/focused-youtube/[edge-extension-id]"
+  firefox: "https://addons.mozilla.org/en-US/firefox/addon/focused-for-youtube",
+  edge: "https://microsoftedge.microsoft.com/addons/detail/fkipbhecgloafeodihbhahjnhmainlil"
 }
 
 const reviewUrls = {
   chromium: "https://chromewebstore.google.com/detail/focused-youtube/nfghbmabdoakhobmimnjkamfdnpfammn/reviews?hl=en",
-  firefox: "https://addons.mozilla.org/en-US/firefox/addon/focused-youtube/reviews",
-  edge: "https://microsoftedge.microsoft.com/addons/detail/focused-youtube/[edge-extension-id]"
+  firefox: "https://addons.mozilla.org/en-US/firefox/addon/focused-for-youtube/reviews/",
+  edge: "https://microsoftedge.microsoft.com/addons/detail/fkipbhecgloafeodihbhahjnhmainlil"
 }
 
 const currentPlatform = process.env.MODE || "chromium"
